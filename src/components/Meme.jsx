847a94761b96613c78928c8ecec8c27acc17a82e
tyVespA <migliavacca.marco08@gmail.com>
2023-02-16 12:39:55 +0100

    Create meme state and display it on page

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,21 +3,41 @@ import s from "../styles/Meme.module.css";
 import MemeData from "../styles/helpers/MemeData";
 
 function Meme() {
-  function callAPI(e) {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, getAllMemeImages] = React.useState(MemeData);
+
+  function getMeme(e) {
     e.preventDefault();
-    console.log(MemeData.data.memes[5].url);
+    const memeArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+
+    const topText = meme.topText;
+    const bottomText = meme.bottomText;
+
+    const randomUrl = memeArray[randomNumber].url;
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: randomUrl,
+    }));
   }
 
   return (
     <div className={s.memeContainer}>
       <form action="">
-        <button onClick={callAPI} type="submit">
+        <button onClick={getMeme} type="submit">
           Get new random image
         </button>
         <div className={s.inputContainer}>
           <input type="text" name="" id="" placeholder="Top text" />
           <input type="text" name="" id="" placeholder="Bottom text" />
         </div>
+        <img src={meme.randomImage} alt="" className={s.memeImage} />
       </form>
     </div>
   );
